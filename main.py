4809a34cadee125107a6a4b4aa784c127e33bacd
Divider <it-cio@mail.ru>
2022-06-02 16:45:33 +0300

    Fix startup submodules

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+from asyncio import set_event_loop, new_event_loop
 
 import aioschedule
 
@@ -6,19 +7,24 @@ from service import weather, covid
 from sql.crud import create_table, drop_table
 from telegram.bot import bot_run
 
+set_event_loop(new_event_loop())
+
 
 async def scheduler():
     aioschedule.every(15).to(30).seconds.do(weather, greet='Прогноз погоды: ')
     aioschedule.every(30).seconds.do(covid, greet='Коронавирус\nоперативные данные\n\n')
     while True:
         await aioschedule.run_pending()
-        await asyncio.sleep(1)
+        await asyncio.sleep(0.1)
 
 
 async def tasks():
     asyncio.create_task(scheduler())
     # asyncio.create_task(routes.ami_listener())
-    await asyncio.sleep(1)
+    await asyncio.sleep(0.1)
+
+
+# asyncio.run(tasks())
 asyncio.get_event_loop().run_until_complete(tasks())
 
 
--- service.py
+++ service.py
@@ -1,7 +1,8 @@
 import asyncio
 
-from asterisk import ami
+# from asterisk import ami
 from covid.prognosis import covid_request
+# from phone.dictionary import dictionary_request
 from sql import crud
 from telegram import bot
 from weather.forecast import weather_request
@@ -38,17 +39,31 @@ async def covid(greet):
             await asyncio.sleep(1)
 
 
-async def ami_listener():
-    ami.connect(state=False)
-    while True:
-        if ami.event:
-            print(ami.event[0])
-            ami.event = []
-        elif ami.caller and ami.number:
-            call_id = await bot.send_message(f'Incoming call\nfrom number: {ami.number[0]}\nto number: {ami.caller[0]}')
-            ami.number, ami.caller = [], []
-        elif ami.status:
-            await bot.delete_message(call_id)
-            bot.id_list.remove(call_id)
-            ami.status = []
-        await asyncio.sleep(1)
+# async def phonebook(greet):
+#     phone = await dictionary_request()
+#     sql_prognosis, sql_id = await asyncio.create_task(crud.select('phone'))
+#
+#     if phone != sql_prognosis:
+#         try:
+#             phone_id = await bot.send_message(greet + phone)
+#             await asyncio.create_task(crud.update('covid', phone, phone_id))
+#         except Exception as ex:
+#             print(ex)
+#         finally:
+#             await asyncio.sleep(1)
+#
+#
+# async def ami_listener():
+#     ami.connect(state=False)
+#     while True:
+#         if ami.event:
+#             print(ami.event[0])
+#             ami.event = []
+#         elif ami.caller and ami.number:
+#             call_id = await bot.send_message(f'Incoming call\nfrom number: {ami.number[0]}\nto number: {ami.caller[0]}')
+#             ami.number, ami.caller = [], []
+#         elif ami.status:
+#             await bot.delete_message(call_id)
+#             bot.id_list.remove(call_id)
+#             ami.status = []
+#         await asyncio.sleep(1)
