652096cbd08ea9e9ebde8b0016b3e753cb961dfb
Divider <it-cio@mail.ru>
2022-06-08 15:23:41 +0300

    Add script for autorun

---
+++ script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# sudo chmod +x script.sh
+clear
+echo ""
+
+echo "==> Update main from Git"
+git reset --hard
+git fetch
+git pull
+echo ""
+
+echo "==> Update submodules from Git"
+git submodules update --remote
+echo ""
+
+echo "==> Check and update dependencies"
+python3 -m poetry update
+echo ""
+
+echo "==> Run server"
+python3 main.py
